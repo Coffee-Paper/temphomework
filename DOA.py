c05d26344dd416018d86ed90e8bc2e3fd5bc2e80
Coffee-Paper <60168864+Coffee-Paper@users.noreply.github.com>
2024-10-20 22:54:46 +0800

    Add Espirit, space_smooth, DML
    add some switch for plot canvas

--- DOA.py
+++ DOA.py
@@ -1,18 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, CheckButtons
 from beamforming import beamform_alg
 
 # 生成功率-角度数据
-def generate_y(angle_in,SNR,N_array,snaps):
+def generate_y(angle_in,SNR,N_array,snaps,sig_type,space_smooth,eq_array):
     ########## 此处调节信噪比等参数
-    instanceWF = beamform_alg(f0=1.5e9,theta_in=angle_in,snr=SNR,signal_type=1,N_array=N_array,L1=snaps)
+    instanceWF = beamform_alg(f0=1.5e9,
+                              theta_in=angle_in,
+                              snr=SNR,
+                              signal_type=sig_type,
+                              N_array=N_array,
+                              L1=snaps,
+                              space_smooth=space_smooth,
+                              eq_array=eq_array)
     ########## signal_type=1使得前两个信号为相关信号，取0则全是非相干信号
-    return  instanceWF.CBF(),instanceWF.MVDR(),instanceWF.MUSIC()
+    return  instanceWF.CBF(),instanceWF.MVDR(),instanceWF.MUSIC(),instanceWF.Espirit(),instanceWF.DML()
 
 # 初始化图形和坐标轴
-fig, ax = plt.subplots(figsize=(8, 6))
-plt.subplots_adjust(bottom=0.35)
+fig, ax = plt.subplots(figsize=(9, 7))
+plt.subplots_adjust(bottom=0.4,right=0.7)
 
 # 初始角度偏移量
 angle_offset = -30
@@ -23,38 +30,58 @@ angle_offset3 = 60
 SNR_init = 10
 N_array_init = 16
 snaps_init = 1024
+sig_type = 1
+space_smooth = False
+eq_array = 8
 
 theta = np.arange(-90, 90, 0.1)
-P_CBF,P_MVDR,P_MUSIC = generate_y([angle_offset,angle_offset2,angle_offset3],SNR_init,N_array_init,snaps_init)
+P_CBF,P_MVDR,P_MUSIC,P_Espirit,P_DML = generate_y([angle_offset,angle_offset2,angle_offset3],
+                                                  SNR_init,
+                                                  N_array_init,
+                                                  snaps_init,
+                                                  sig_type,
+                                                  space_smooth,
+                                                  eq_array)
 line_CBF, = ax.plot((theta), P_CBF, lw=1,label='CBF')
 line_MVDR, = ax.plot((theta), P_MVDR, lw=1,label='MVDR')
 line_MUSIC, = ax.plot((theta), P_MUSIC, lw=1,label='MUSIC')
+Dot_Espirit = ax.scatter(P_Espirit, np.zeros(len(P_Espirit)), marker='x', label='Espirit')
+line_DML, = ax.plot((theta), P_DML, lw=1,label='DML')
 ax.set_title('Power-Angle Pattern')
 ax.set_xlabel('Angle (degrees)')
 ax.set_ylabel('Power (dB)')
 ax.grid()
 ax.set_xlim([-90, 90])
 ax.set_ylim([-100, 10])
-ax.legend()
+ax.legend(bbox_to_anchor=(1.05, 1))
 
 # 添加滑块
-ax_slider = plt.axes([0.1, 0.25, 0.8, 0.03])
+ax_slider = plt.axes([0.1, 0.30, 0.8, 0.03])
 slider = Slider(ax_slider, 'Angle1', -90, 90, valinit=angle_offset,valstep=0.1)
 
-ax_slider2 = plt.axes([0.1, 0.20, 0.8, 0.03])
+ax_slider2 = plt.axes([0.1, 0.25, 0.8, 0.03])
 slider2 = Slider(ax_slider2, 'Angle2', -90, 90, valinit=angle_offset2,valstep=0.1)
 
-ax_slider3 = plt.axes([0.1, 0.15, 0.8, 0.03])
+ax_slider3 = plt.axes([0.1, 0.20, 0.8, 0.03])
 slider3 = Slider(ax_slider3, 'Angle3', -90, 90, valinit=angle_offset3,valstep=0.1)
 
-ax_slider4 = plt.axes([0.1, 0.10, 0.8, 0.03])
+ax_slider4 = plt.axes([0.1, 0.15, 0.8, 0.03])
 slider4 = Slider(ax_slider4, 'SNR', 1, 30, valinit=SNR_init,valstep=0.1)
 
-ax_slider5 = plt.axes([0.1, 0.05, 0.8, 0.03])
+ax_slider5 = plt.axes([0.1, 0.10, 0.8, 0.03])
 slider5 = Slider(ax_slider5, 'N_array', 8, 32, valinit=N_array_init,valstep=1)
 
-ax_slider6 = plt.axes([0.1, 0.0, 0.8, 0.03])
+ax_slider6 = plt.axes([0.1, 0.05, 0.8, 0.03])
 slider6 = Slider(ax_slider6, 'snaps', 100, 1000, valinit=snaps_init,valstep=1)
+
+ax_slider7 = plt.axes([0.1, 0.00, 0.8, 0.03])
+slider7 = Slider(ax_slider7, 'eq_array', 1, 32, valinit=eq_array,valstep=1)
+
+rax = plt.axes([0.75, 0.4, 0.15, 0.1])  # 开关放在画布右侧
+check = CheckButtons(rax, ['Signal Type'], [False])
+
+rax2 = plt.axes([0.75, 0.5, 0.15, 0.1])  # 开关放在画布右侧
+check2 = CheckButtons(rax2, ['Space Smooth'], [False])
 
 # 更新函数
 def update(val):
@@ -64,11 +91,39 @@ def update(val):
     SNR = (slider4.val)
     N_array = (slider5.val)
     snaps = (slider6.val)
-    P_CBF,P_MVDR,P_MUSIC = generate_y([angle,angle2,angle3],SNR=SNR,N_array=N_array,snaps=snaps)    
+    eq_array = (slider7.val)
+    P_CBF,P_MVDR,P_MUSIC,P_Espirit,P_DML = generate_y([angle,angle2,angle3],
+                                                      SNR=SNR,
+                                                      N_array=N_array,
+                                                      snaps=snaps,
+                                                      sig_type=sig_type,
+                                                      space_smooth=space_smooth,
+                                                      eq_array=eq_array
+                                                      )    
     line_CBF.set_ydata(P_CBF)
     line_MVDR.set_ydata(P_MVDR)
     line_MUSIC.set_ydata(P_MUSIC)
+    Dot_Espirit.set_offsets(np.c_[P_Espirit,np.zeros(len(P_Espirit))])
+    line_DML.set_ydata(P_DML)
     fig.canvas.draw_idle()
+
+def toggle_signal_type(val):
+    global sig_type
+    if sig_type == 0:
+        sig_type = 1
+    else:
+        sig_type = 0  # 切换信号类型
+    update(val)
+    
+def toggle_space_smooth(val):
+    global space_smooth
+    if space_smooth == False:
+        ax.set_ylim([-200, 10])
+        space_smooth = True
+    else:
+        ax.set_ylim([-100, 10])
+        space_smooth = False  # 切换信号类型
+    update(val)
 
 # 连接滑块事件
 slider.on_changed(update)
@@ -77,5 +132,9 @@ slider3.on_changed(update)
 slider4.on_changed(update)
 slider5.on_changed(update)
 slider6.on_changed(update)
+slider7.on_changed(update)
+check.on_clicked(toggle_signal_type)
+check2.on_clicked(toggle_space_smooth)
+
 # 显示窗口
 plt.show()
--- beamforming.py
+++ beamforming.py
@@ -1,63 +1,177 @@
 import numpy as np
 
 class beamform_alg:
-    
-    def __init__(self,f0=1.5e9,theta_in=[],N_array=32,L1=10000,snr=20,angle_step=0.1,sample_rate=1e6,signal_type=0):
-        self.c = 3e8                     #光速
-        self.t = np.arange(L1)/sample_rate          #时间向量
-        self.N = np.arange(0,N_array,1).reshape([-1,1])   #阵元数目矩阵
-        self.theta_t = np.array(theta_in)       #信源入射角度
-        self.num_t = len(self.theta_t)          #信源数目
-        self.lambda0 = self.c/f0              #波长
-        self.d = self.lambda0*0.5               #阵元间隔
-        self.theta_scan = np.arange(-90, 90, angle_step).reshape([1,-1])   #角度
-        self.sample_rate = sample_rate          #信号采样率
-        self.steer_vector = np.exp(-2j*np.pi * self.N * self.d/self.lambda0 * np.sin(np.deg2rad(self.theta_t)))
-        self.sin_signal = np.tile(np.exp(2j*np.pi*f0*self.t),(self.num_t,1))  #单频信号
-        self.rand_signal = np.random.randn(self.num_t,L1)  #随机信号
-        self.signal = self.sin_signal if signal_type == 1 else self.rand_signal 
-        self.signal[-1,:] = self.rand_signal[-1,:]*1.414   #最后一个信号不同
-        self.noise = np.random.randn(self.N.shape[0],L1) + 1j*np.random.randn(self.N.shape[0],L1)
-        self.snr_ratio = (1/(10**(snr/10)))**0.5
-        self.X = (self.steer_vector @ self.signal) + self.snr_ratio * self.noise
-        self.R = self.X @ self.X.T.conj()/self.X.shape[1]
 
+    def __init__(
+        self,
+        f0=1.5e9,
+        theta_in=[],
+        N_array=32,
+        L1=10000,
+        snr=20,
+        angle_step=0.1,
+        sample_rate=1e6,
+        signal_type=0,
+        space_smooth=True,
+        eq_array=8
+    ):
+        self.L1 = L1
+        self.c = 3e8                                                       # 光速
+        self.t = np.arange(L1) / sample_rate                               # 时间向量
+        self.N_array = N_array                                             # 阵元数目
+        self.N = np.arange(0, N_array, 1).reshape([-1, 1])                 # 阵元数目矩阵
+        self.theta_t = np.array(theta_in)                                  # 信源入射角度
+        self.num_t = len(self.theta_t)                                     # 信源数目
+        self.lambda0 = self.c / f0                                         # 波长
+        self.d = self.lambda0 * 0.5                                        # 阵元间隔
+        self.theta_scan = np.arange(-90, 90, angle_step).reshape([1, -1])  # 角度
+        self.sample_rate = sample_rate                                     # 信号采样率
+        self.eq_array = eq_array                                           # 等效阵元数
+        self.steer_vector = np.exp(                                        # 导向矢量
+            -2j
+            * np.pi
+            * self.N
+            * self.d
+            / self.lambda0
+            * np.sin(np.deg2rad(self.theta_t))
+        )
+        self.sin_signal = np.tile(
+            np.exp(2j * np.pi * f0 * self.t), (self.num_t, 1)
+        )                                                                           # 单频信号
+        self.rand_signal = np.random.randn(self.num_t, L1)                          # 随机信号
+        self.signal = self.sin_signal if signal_type == 1 else self.rand_signal
+        self.signal[-1, :] = self.rand_signal[-1, :]
+        self.noise = np.random.randn(self.N.shape[0], L1) + 1j * np.random.randn(
+            self.N.shape[0], L1
+        )
+        self.snr_ratio = (1 / (10 ** (snr / 10))) ** 0.5
+        self.steer_sig = self.steer_vector @ self.signal
+        self.X = self.steer_sig + self.snr_ratio * self.noise    # X为含噪声信号
+        self.R0 = self.X @ self.X.T.conj() / self.X.shape[1]                         # 自相关矩阵
+        self.R = self._space_smooth_alg() if space_smooth else self.R0
+        
+    def _space_smooth_alg(self,):
+        smooth_array_num = self.N_array - self.eq_array + 1
+        R_sig = np.zeros([self.eq_array,self.eq_array])
+        for i in range(smooth_array_num):
+            sub = self.steer_sig[i:self.eq_array+i,:]
+            R_sig = R_sig + (sub @ sub.T.conj() / sub.shape[1])
+        R_sig = R_sig / smooth_array_num
+        
+        self.N_array = self.eq_array
+        self.N = np.arange(0, self.N_array, 1).reshape([-1, 1])
+        self.steer_vector = np.exp(                                        # 导向矢量
+            -2j
+            * np.pi
+            * self.N
+            * self.d
+            / self.lambda0
+            * np.sin(np.deg2rad(self.theta_t))
+        )
+        self.noise = np.random.randn(self.N.shape[0], self.L1) + 1j * np.random.randn(
+            self.N.shape[0], self.L1
+        )
+        self.steer_sig = self.steer_vector @ self.signal
+        self.X = self.steer_sig + self.snr_ratio * self.noise    # X为含噪声信号
+        return R_sig
+     
+    
     def CBF(self):
         yy = []
         for th in self.theta_scan[0]:
-            steervec = np.exp(-2j * np.pi / self.lambda0 * self.N * self.d * np.sin(np.deg2rad(th)))  # 每个角度上的方向矢量
+            steervec = np.exp(
+                -2j * np.pi / self.lambda0 * self.N * self.d * np.sin(np.deg2rad(th))
+            )  # 每个角度上的方向矢量
             result = np.abs(steervec.T.conj() @ self.R @ steervec)
             yy.append(result)
-        yy = yy/np.max(yy)
-        y = 10*np.log10(np.array(yy).reshape([-1,1]))
+        yy = yy / np.max(yy)
+        y = 10 * np.log10(np.array(yy).reshape([-1, 1]))
         return y
-    
+
     def MVDR(self):
         Rinv = np.linalg.inv(self.R)
         yy = []
         for th in self.theta_scan[0]:
-            steervec = np.exp(-2j * np.pi * self.d/self.lambda0 * self.N * np.sin(np.deg2rad(th))) # 每个角度上的方向矢量
-            result = np.abs(1/(steervec.T.conj() @ Rinv @ steervec))
+            steervec = np.exp(
+                -2j * np.pi * self.d / self.lambda0 * self.N * np.sin(np.deg2rad(th))
+            )  # 每个角度上的方向矢量
+            result = np.abs(1 / (steervec.T.conj() @ Rinv @ steervec))
             yy.append(result)
-        yy = yy/np.max(yy)
-        y = 10*np.log10(np.array(yy).reshape([-1,1]))        
+        yy = yy / np.max(yy)
+        y = 10 * np.log10(np.array(yy).reshape([-1, 1]))
         return y
-    
-    def MUSIC(self,expected_sig_num = 3):
-        '''Adjusting num_expected_signals between 1 and 7, 
-           underestimating the number will lead to missing signal(s)
-           while overestimating will only slightly hurt performance.'''
-        
-        w, v = np.linalg.eig(self.R) # 特征值分解，v[:,i]是特征值w[i]对应的特征向量
-        eig_order = np.argsort(np.abs(w)) # 升序排列，eig_order是对应的索引序号
-        v = v[:,eig_order] # 重新排序v
-        V = v[:,0:len(self.N)-expected_sig_num]
-        
-        yy = []  
+      
+    def MUSIC(self, expected_sig_num=3):
+        """Adjusting num_expected_signals between 1 and 7,
+        underestimating the number will lead to missing signal(s)
+        while overestimating will only slightly hurt performance."""
+
+        w, v = np.linalg.eig(self.R)            # 特征值分解，v[:,i]是特征值w[i]对应的特征向量
+        eig_order = np.argsort(np.abs(w))       # 升序排列，eig_order是对应的索引序号
+        v = v[:, eig_order]                     # 重新排序v
+        V = np.zeros((len(self.N), len(self.N) - expected_sig_num), dtype=np.complex64)
+        for i in range(len(self.N) - expected_sig_num):
+            V[:, i] = v[:, i]
+
+        yy = []
+
         for th in self.theta_scan[0]:
-            steervec = np.exp(-2j * np.pi * self.d/self.lambda0 * self.N * np.sin(np.deg2rad(th))) # 每个角度上的方向矢量
-            value = np.abs((1/(steervec.T.conj() @ V @ V.T.conj() @ steervec)).squeeze()) # MUSIC
+            steervec = np.exp(
+                -2j * np.pi * self.d / self.lambda0 * self.N * np.sin(np.deg2rad(th))
+            )  # 每个角度上的方向矢量
+            value = np.abs(
+                (1 / (steervec.T.conj() @ V @ V.T.conj() @ steervec)).squeeze()
+            )  # MUSIC
             yy.append(value)
-        yy = yy/np.max(yy)
-        y = 10*np.log10(np.array(yy).reshape([-1,1]))
+        yy = yy / np.max(yy)
+        y = 10 * np.log10(np.array(yy).reshape([-1, 1]))
+        return y
+
+    def Espirit(self,):
+        """Espirit算法，旋转子空间不变算法，返回估计的角度，有相干源时效果很差
+
+        Returns:
+            ndarray: shape[num_of_signals,1], 返回的直接是估计的角度
+        """
+        X1 = self.X[:self.N_array-1,:]
+        X2 = self.X[1:self.N_array,:]
+        XX = np.vstack((X1,X2))
+        RX = XX @ XX.T.conj() / XX.shape[1]
+        w , v = np.linalg.eig(RX)
+        Us = v[:,:self.num_t]
+        Us1 = Us[:self.N_array-1,:]
+        Us2 = Us[self.N_array-1:,:]
+        PSI = np.linalg.inv(Us1.T.conj() @ Us1) @ Us1.T.conj() @ Us2    # 求LS下的PSI矩阵并求其特征值
+        W , V =np.linalg.eig(PSI)
+        ephi=np.arctan2(np.imag(W),np.real(W))
+        P = np.rad2deg(-np.arcsin(ephi/np.pi))
+        return P
+    
+    def DML(self,):
+        """DML算法，曲线极小值处为估计角度
+
+        Returns:
+            y: ndarray[1800,1]
+        """
+        steervec = np.exp(-2j * np.pi / self.lambda0 * self.N * self.d * np.sin(np.deg2rad(self.theta_scan)))
+        scan_len = steervec.shape[1]
+        y = np.zeros([scan_len,1],dtype=complex)
+        for i in range(0,scan_len):
+            scan = steervec[:,i].reshape([-1,1])
+            Pa = scan / (scan.T.conj() @ scan) @ scan.T.conj()
+            oPa = np.eye(Pa.shape[0]) - Pa
+            y[i] = np.abs(np.trace( oPa @ self.R) / self.N_array)
+        y = y / np.max(y)
+        y = 10 * np.log10(y)
+        
+        
+        # steervec = np.exp(-2j * np.pi / self.lambda0 * self.N * self.d * np.sin(np.deg2rad(self.theta_scan)))
+        # yy = []
+        # for index in range(steervec.shape[1]):
+        #     vec = steervec[:,index].reshape([-1,1])
+        #     Pi_A_theta = vec @ np.linalg.pinv(vec)
+        #     oPi_A_theta = np.eye(Pi_A_theta.shape[0]) - Pi_A_theta
+        #     yy.append(np.abs(np.trace(oPi_A_theta @ self.R) / self.N_array))
+        # yy = yy / np.max(yy)
+        # y = 10 * np.log10(np.array(yy).reshape([-1, 1]))
         return y
